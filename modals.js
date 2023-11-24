da6f7f8239c7ecf5c5bd59a3dc071beb9beac24d
John <john.monsalve94@gmail.com>
2023-11-23 22:44:12 -0500

    Primer subida

---
+++ modals.js
@@ -0,0 +1,9 @@
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
